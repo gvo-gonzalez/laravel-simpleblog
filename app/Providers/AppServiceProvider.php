06741864e0d23ff3c013e415801a32b18f6deba2
gvo.gonzalez <gvo.gonzalez@gmail.com>
2020-09-03 19:04:06 +0000

    project setup

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Validator;
+use App\Services\Validation;
+
+class AppServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		If (env('APP_ENV') !== 'local') {
+			$this->app['request']->server->set('HTTPS', true);
+		}
+
+		Validator::resolver(function($translator, $data, $rules, $messages)
+		{
+		    return new Validation($translator, $data, $rules, $messages);
+		});
+	}
+
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+}
